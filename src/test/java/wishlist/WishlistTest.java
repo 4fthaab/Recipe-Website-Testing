d35c00904a935ad609dd5ef301ee181e91c9f197
4fthaab <afthabr88@gmail.com>
2026-09-08 12:54:43 +0530

    wishlist page testcases implemented

--- src/test/java/wishlist/WishlistTest.java
+++ src/test/java/wishlist/WishlistTest.java
@@ -1,4 +1,13 @@
 package wishlist;
 
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+        FunctionalTest.class,
+        UITest.class
+})
+
 public class WishlistTest {
 }
